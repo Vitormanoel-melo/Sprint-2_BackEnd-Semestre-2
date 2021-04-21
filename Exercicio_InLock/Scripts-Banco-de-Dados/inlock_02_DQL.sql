1705cd42dedac347d56a489595717f3743a316ad
Vitor Manoel Melo <vitormmbrito12@gmail.com>
2021-04-21 01:23:14 -0300

    Exercício InLock quase finalizado - faltando swagger

--- Exercicio_InLock/Scripts-Banco-de-Dados/inlock_02_DQL.sql
+++ Exercicio_InLock/Scripts-Banco-de-Dados/inlock_02_DQL.sql
@@ -22,6 +22,7 @@ GO
 SELECT nomeJogo, nomeEstudio FROM jogos
 INNER JOIN estudios
 ON jogos.idEstudio = estudios.idEstudio;
+GO
 
 
 -- Buscar e trazer na lista todos os estúdios com os respectivos jogos. Obs.: Listar
@@ -29,38 +30,45 @@ ON jogos.idEstudio = estudios.idEstudio;
 SELECT nomeEstudio, nomeJogo FROM estudios
 LEFT JOIN jogos
 ON estudios.idEstudio = jogos.idEstudio;
+GO
 
 
 -- Buscar um usuário por e-mail e senha (login)
 SELECT idUsuario, email, senha, idTipoUsuario FROM usuarios
 WHERE email = 'cliente@cliente.com' AND senha = 'cliente';
+GO
 
 
 -- Buscar um jogo por idJogo
 SELECT idJogo, nomeJogo, descricao, dataLancamento, valor, idEstudio FROM jogos
 WHERE idJogo = 1;
+GO
 
 
 -- Buscar um estúdio por idEstudio
 SELECT idEstudio, nomeEstudio FROM estudios
 WHERE idEstudio = 1;
-
+GO
 
 
 -- Comandos Adicionais
 
 SELECT * FROM tiposUsuarios;
+GO
 
 
 SELECT idJogo, nomeJogo, descricao, dataLancamento, valor, jogos.idEstudio, nomeEstudio FROM jogos
 INNER JOIN estudios
 ON jogos.idEstudio = estudios.idEstudio;
+GO
 
 
 SELECT * FROM jogos
 WHERE idEstudio = 1;
+GO
 
 
+-- Mostrar a lista de todos os estúdios e incluir na lista a lista de jogos daquele determinado estúdio
 SELECT idJogo, nomeJogo, descricao, dataLancamento, valor, jogos.idEstudio, estudios.idEstudio, nomeEstudio FROM jogos
 INNER JOIN estudios
 ON jogos.idEstudio = estudios.idEstudio
---
+++ Exercicio_InLock/senai.inlock.webApi/senai.inlock.webApi/Repositories/LoginRepository.cs
@@ -0,0 +1,56 @@
+﻿using senai.inlock.webApi.Domains;
+using senai.inlock.webApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.inlock.webApi.Repositories
+{
+    public class LoginRepository : ILoginRepository
+    {
+        private string stringConexao = "Data Source=LAPTOP-70KR9CNR; initial catalog=inlock_games_tarde; user Id=sa; pwd=senai@132";
+
+
+        /// <summary>
+        /// Busca um usuário pelo email e senha
+        /// </summary>
+        /// <param name="email">email do usuário buscado</param>
+        /// <param name="senha">senha do usuário buscado</param>
+        /// <returns>Usuário buscado</returns>
+        public UsuarioDomain BuscarEmailSenha(string email, string senha)
+        {
+            using (SqlConnection con = new SqlConnection(stringConexao))
+            {
+                string querySelect = "SELECT idUsuario, email, senha, idTipoUsuario FROM usuarios WHERE email = @email AND senha = @senha";
+
+                using (SqlCommand cmd = new SqlCommand(querySelect, con))
+                {
+                    con.Open();
+
+                    cmd.Parameters.AddWithValue("@email", email);
+                    cmd.Parameters.AddWithValue("@senha", senha);
+
+                    SqlDataReader rdr = cmd.ExecuteReader();
+
+                    if (rdr.Read())
+                    {
+                        UsuarioDomain usuario = new UsuarioDomain
+                        {
+                            idUsuario = Convert.ToInt32(rdr["idUsuario"]),
+                            email = rdr["email"].ToString(),
+                            senha = rdr["senha"].ToString(),
+                            idTipoUsuario = Convert.ToInt32(rdr["idTipoUsuario"])
+                        };
+
+                        return usuario;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+    }
+}
