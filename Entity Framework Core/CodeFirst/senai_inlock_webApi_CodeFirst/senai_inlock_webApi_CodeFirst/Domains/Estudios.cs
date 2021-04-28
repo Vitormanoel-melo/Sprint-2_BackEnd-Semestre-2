a10fdead9e8e85167b5216b2582f474c1f1338ac
Vitor Manoel Melo <vitormmbrito12@gmail.com>
2021-04-28 02:08:08 -0300

    Estudos de Code First adicionados na pasta Entity Framework Core

---
+++ Entity Framework Core/CodeFirst/senai_inlock_webApi_CodeFirst/senai_inlock_webApi_CodeFirst/Domains/Estudios.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace senai_inlock_webApi_CodeFirst.Domains
+{
+    [Table("Estudios")]
+    public class Estudios
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int idEstudio { get; set; }
+
+        [Column(TypeName = "VARCHAR(150)")]
+        [Required(ErrorMessage = "O nome do estúdio é obrigatório!")]
+        public string nomeEstudio { get; set; }
+        
+        // propriedade que vai ser utilizada apenas pela API, não vai para o banco
+        public List<Jogos> jogos { get; set; }
+    }
+}
